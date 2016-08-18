82c0a5d10b7ff00e8db78c4a83c86a47973a294a
Tanki <tanki_jong@hotmail.com>
2016-08-18 22:52:12 +0800

    add hint word

--- app/scripts/component/app.vue
+++ app/scripts/component/app.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="app" :class="{cover: notActive}">
+  <div class="app" :class="{cover: !isActive}">
     <div class="info-tab">
-      <div class="info-tab-header" :class="{animate: !notActive}">
-          <div class="name-box-wrapper">
-            <div class="name-box">
-              <div class="avatar-container" @click="toggle">
-                <div class="avatar"></div>
-              </div>
-              <h1>Tanki Zhang</h1>
-              <h3>- Full Stack Engineer -</h3>
-              <div class="contacts">
-                <span><a href="mailto: tanki_jong@hotmail.com"><i class="demo-icon icon-mail"></i></a></span>
-                <span><a href="http://github.com/tankijong"><i class="demo-icon icon-github-circled"></i></a></span>
-                <span><a href="https://www.linkedin.com/in/tankijong"><i class="demo-icon icon-linkedin"></i></a></span>
+      <div class="info-tab-header" :class="{animate: isActive}">
+        <div class="name-box-wrapper">
+          <div class="name-box">
+            <div class="avatar-container" @click="toggle">
+              <div class="avatar"></div>
             </div>
+            <h1>Tanki Zhang</h1>
+            <h3>- Full Stack Engineer -</h3>
+            <div class="contacts">
+              <span><a href="mailto: tanki_jong@hotmail.com"><i class="demo-icon icon-mail"></i></a></span>
+              <span><a href="http://github.com/tankijong"><i class="demo-icon icon-github-circled"></i></a></span>
+              <span><a href="https://www.linkedin.com/in/tankijong"><i class="demo-icon icon-linkedin"></i></a></span>
+
+            </div>
+            <div class="hint-word">Clicking my avatar to know more about me ;)</div>
           </div>
         </div>
       </div>
-      <div class="info-tab-content" :class="{animate: !notActive}">
+      <div class="info-tab-content" :class="{animate: isActive}">
         Hello, I'm a full stack engineer @ <a href="http://daocloud.io">daocloud</a>.
         <skill-stack></skill-stack>
         <tabs></tabs>
@@ -31,11 +33,11 @@ import skillStack from './skill-stack.vue';
 import tabs from './tabs.vue';
 export default {
   data:()=>({
-    notActive: true,
+    isActive: false,
   }),
   methods:{
     toggle(){
-      this.notActive = !this.notActive;
+      this.isActive = !this.isActive;
     }
   },
   components:{
